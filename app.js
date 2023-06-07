7af43f3c0ae859522d5e4ff9429b5687976f60d6
AhronsVianna <ahronsvianna@gmail.com>
2023-06-07 18:35:06 -0300

    commit pelo git

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ console.log("Rodando testes para verificação");
 console.log("Precisa praticar para aprimorar");
 // testes sem fim
 console.log("inseridos no primeiro commit");
+// inclusão pelo git
+console.log("pratica");
